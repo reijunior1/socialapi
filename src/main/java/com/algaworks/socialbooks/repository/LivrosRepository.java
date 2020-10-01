c08e3fe3d737fc0dfa66f61f4cfafba5038b9171
Reinaldo Couto <rcouto@blupay.com.br>
2020-10-01 11:06:48 -0300

    Manipulando erros com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/algaworks/socialbooks/repository/LivrosRepository.java
+++ src/main/java/com/algaworks/socialbooks/repository/LivrosRepository.java
@@ -1,9 +1,11 @@
 package com.algaworks.socialbooks.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.socialbooks.domain.Livro;
 
+@Repository
 public interface LivrosRepository extends JpaRepository<Livro, Long>{
 
 }
